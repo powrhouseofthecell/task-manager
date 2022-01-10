42aa8ee56d34ebce8bfbaafffb1c3545d71d5050
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-10 17:49:37 +0530

    Update env

--- src/email/account.js
+++ src/email/account.js
@@ -7,7 +7,7 @@ sgMail.setApiKey(sendgridAPIKey);
 const sendWelcomeEmail = (email, name) => {
    sgMail.send({
       to: email,
-      from: 'theshahzuhaib96@gmail.com',
+      from: process.env.MAIL_FROM,
       subject: 'Thanks for joining in!',
       text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
    });
@@ -16,7 +16,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
    sgMail.send({
       to: email,
-      from: 'theshahzuhaib96@gmail.com',
+      from: process.env.MAIL_FROM,
       subject: 'Sorry to see you go!',
       text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
    });
