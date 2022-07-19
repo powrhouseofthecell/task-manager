8310fbff66caed1817e241f4c82fe31d0ec433d3
Zuhaib <theshahzuhaib96@gmail.com>
2022-07-19 15:05:06 +0530

    prettier

--- src/routers/user.js
+++ src/routers/user.js
@@ -34,7 +34,7 @@ router.post('/users/login', async (req, res) => {
 
 router.post('/users/logout', auth, async (req, res) => {
    try {
-      req.user.tokens = req.user.tokens.filter((token) => {
+      req.user.tokens = req.user.tokens.filter(token => {
          return token.token !== req.token;
       });
       await req.user.save();
@@ -62,7 +62,7 @@ router.get('/users/me', auth, async (req, res) => {
 router.patch('/users/me', auth, async (req, res) => {
    const updates = Object.keys(req.body);
    const allowedUpdates = ['name', 'email', 'password', 'age'];
-   const isValidOperation = updates.every((update) =>
+   const isValidOperation = updates.every(update =>
       allowedUpdates.includes(update)
    );
 
@@ -71,7 +71,7 @@ router.patch('/users/me', auth, async (req, res) => {
    }
 
    try {
-      updates.forEach((update) => (req.user[update] = req.body[update]));
+      updates.forEach(update => (req.user[update] = req.body[update]));
       await req.user.save();
       res.send(req.user);
    } catch (e) {
